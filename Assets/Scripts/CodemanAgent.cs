fc5922afc1cb1cbd386383426f82ac1473b4c974
Konstantinos Egkarchos <konsnosl@gmail.com>
2019-05-23 13:50:56 +0300

    Added comments and improved navigation scene.

--- Assets/Scripts/CodemanAgent.cs
+++ Assets/Scripts/CodemanAgent.cs
@@ -2,23 +2,31 @@
 using UnityEngine;
 using UnityEngine.AI;
 
-public class CodemanAgent : MonoBehaviour, IInputClickHandler
+public class CodemanAgent : MonoBehaviour
 {
+    ///<summary>Handles the pathfinding for the GameObject.</summary>
     private NavMeshAgent agent;
+    ///<summary>Updates animation.</summary>
     private Animator animator;
 
+    private ConsumeInputClickHandler clickHandler;
+
     private readonly int animatorForward = Animator.StringToHash("Forward");
-    private readonly int animatorTurn = Animator.StringToHash("Turn");
-    
+
     void Awake()
     {
         agent = GetComponent<NavMeshAgent>();
         animator = GetComponent<Animator>();
+        clickHandler = GetComponent<ConsumeInputClickHandler>();
+        clickHandler.OnInputClickedEvent += OnClicked;
 
         agent.updatePosition = false;
         agent.updateRotation = false;
     }
 
+    /// <summary>
+    /// Listen to any input that isn't consumed by other scripts.
+    /// </summary>
     private void Start()
     {
         InputManager.Instance.PushFallbackInputHandler(gameObject);
@@ -26,10 +34,29 @@ public class CodemanAgent : MonoBehaviour, IInputClickHandler
 
     private void Update()
     {
-        UpdateAnimator();
+        UpdateAnimatorParameters();
     }
 
-    public void OnInputClicked(InputClickedEventData eventData)
+    /// <summary>
+    /// Callback for processing animation movements for modifying root motion.
+    /// This callback will be invoked at each frame after the state machines and the animations have been evaluated, but before OnAnimatorIK.
+    /// </summary>
+    private void OnAnimatorMove()
+    {
+        transform.position = agent.nextPosition;
+
+        if (agent.velocity.sqrMagnitude > Mathf.Epsilon)
+        {
+            transform.rotation = Quaternion.LookRotation(new Vector3(agent.velocity.x, 0f, agent.velocity.z));
+        }
+    }
+
+    private void OnDestroy()
+    {
+        clickHandler.OnInputClickedEvent -= OnClicked;
+    }
+
+    public void OnClicked(InputClickedEventData eventData)
     {
         Vector3 hitPoint;
         Transform camTransform = Camera.main.transform;
@@ -44,18 +71,11 @@ public class CodemanAgent : MonoBehaviour, IInputClickHandler
         }
     }
 
-    private void UpdateAnimator()
+    /// <summary>
+    /// Updates real time animator parameters
+    /// </summary>
+    private void UpdateAnimatorParameters()
     {
         animator.SetFloat(animatorForward, agent.desiredVelocity.magnitude / agent.speed);
-    }
-
-    private void OnAnimatorMove()
-    {
-        transform.position = agent.nextPosition;
-
-        if (agent.velocity.sqrMagnitude > Mathf.Epsilon)
-        {
-            transform.rotation = Quaternion.LookRotation(new Vector3(agent.velocity.x, 0f, agent.velocity.z));
-        }
     }
 }
--- Assets/Scripts/NavMeshBuilder.cs
+++ Assets/Scripts/NavMeshBuilder.cs
@@ -9,12 +9,14 @@ public class NavMeshBuilder : MonoBehaviour
 
     [SerializeField] private NavMeshSurface navMeshSurface;
 
+    ///<summary>Time to wait before building the navigation mesh.</summary>
     private const float waitToBuildNavigation = 0.5f;
-
+    ///<summary>Timestamp to build the navigation mesh.</summary>
     private long buildNavMeshAt = long.MaxValue;
 
     private void Awake()
     {
+        // Add listeners for mesh changes.
         spatialMappingObserver.SurfaceAdded += OnSurfaceAdded;
         spatialMappingObserver.SurfaceUpdated += OnSurfaceUpdated;
         spatialMappingObserver.SurfaceRemoved += OnSurfaceRemoved;
@@ -25,17 +27,22 @@ public class NavMeshBuilder : MonoBehaviour
         ScheduleNavMeshUpdate();
     }
 
-    private void OnDestroy()
-    {
-        spatialMappingObserver.SurfaceUpdated -= OnSurfaceUpdated;
-    }
-
     private void Update()
     {
-        if(DateTime.UtcNow.Ticks > buildNavMeshAt)
+        if (DateTime.UtcNow.Ticks > buildNavMeshAt)
             UpdateNavigationMesh();
     }
 
+    private void OnDestroy()
+    {
+        spatialMappingObserver.SurfaceAdded -= OnSurfaceAdded;
+        spatialMappingObserver.SurfaceUpdated -= OnSurfaceUpdated;
+        spatialMappingObserver.SurfaceRemoved -= OnSurfaceRemoved;
+    }
+
+    /// <summary>
+    /// Creates the navigation mesh.
+    /// </summary>
     private void UpdateNavigationMesh()
     {
         navMeshSurface.buildHeightMesh = true;
@@ -56,15 +63,18 @@ public class NavMeshBuilder : MonoBehaviour
         ScheduleNavMeshUpdate();
     }
 
+    private void OnSurfaceRemoved(object sender, DataEventArgs<SpatialMappingSource.SurfaceObject> e)
+    {
+        ScheduleNavMeshUpdate();
+    }
+
+    /// <summary>
+    /// Schedules a navigation mesh rebuild. It doesn't update it instaly in order to avoid multiple invocations.
+    /// </summary>
     private void ScheduleNavMeshUpdate()
     {
         // Avoid multiple updates
         if (waitToBuildNavigation < DateTime.UtcNow.Ticks)
             buildNavMeshAt = DateTime.UtcNow.Ticks + TimeSpan.FromSeconds(waitToBuildNavigation).Ticks;
-    }
-
-    private void OnSurfaceRemoved(object sender, DataEventArgs<SpatialMappingSource.SurfaceObject> e)
-    {
-        ScheduleNavMeshUpdate();
     }
 }
